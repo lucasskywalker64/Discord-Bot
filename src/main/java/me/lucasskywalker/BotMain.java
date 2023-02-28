482a7745b39b3177e1f94059a5716b0f322eae88
lucasskywalker <lucasskywalker@gmx.de>
2023-02-28 16:04:06 +0100

    Change member count method to be more lightweight (and actually work)

--- src/main/java/me/lucasskywalker/BotMain.java
+++ src/main/java/me/lucasskywalker/BotMain.java
@@ -8,17 +8,14 @@ import me.lucasskywalker.commands.SlashCommandManager;
 import me.lucasskywalker.listeners.ReactionRoleManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.exceptions.InvalidTokenException;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -39,7 +36,6 @@ public class BotMain {
 
     private static String channel;
 
-    private static List<Member> memberList;
 
     public static void scheduleUpdateMemberCount() {
         try {
@@ -59,13 +55,12 @@ public class BotMain {
 
     private static void updateMemberCount() {
         discordAPI.getVoiceChannelById(channel).getManager().setName("Member count: " +
-                memberList.size()).queue();
+                discordAPI.getGuilds().get(0).getMemberCount()).queue();
     }
 
     private static boolean init() throws InvalidTokenException, URISyntaxException {
         discordAPI = JDABuilder.createDefault(CONFIG.get("BOT_TOKEN"))
                 .enableIntents(GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.GUILD_MEMBERS)
-                .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .build();
         discordAPI.addEventListener(new ReactionRoleManager(), new SlashCommandManager());
         File filePath = new File(new File(SlashCommandManager.class.getProtectionDomain()
@@ -88,8 +83,6 @@ public class BotMain {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-
-        memberList = discordAPI.getGuilds().get(0).loadMembers().get();
 
         scheduleUpdateMemberCount();
 
