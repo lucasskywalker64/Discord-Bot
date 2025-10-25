6bf6cf7460a375db10f09ce49cdae819cb1c95e2
lucasskywalker <lucasskywalker@gmx.de>
2025-10-25 18:16:30 +0200

    actually fix xml parsing

--- src/main/java/com/github/lucasskywalker64/api/youtube/model/Entry.java
+++ src/main/java/com/github/lucasskywalker64/api/youtube/model/Entry.java
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 public class Entry {
 
-    @JacksonXmlProperty(localName = "videoId")
+    @JacksonXmlProperty(localName = "videoId", namespace = Feed.YT_NS)
     public String videoId;
 
+    @JacksonXmlProperty(localName = "title", namespace = Feed.ATOM_NS)
     public String title;
 
+    @JacksonXmlProperty(localName = "published", namespace = Feed.ATOM_NS)
     public String published;
 
+    @JacksonXmlProperty(localName = "updated", namespace = Feed.ATOM_NS)
     public String updated;
 
+    @JacksonXmlProperty(localName = "link", namespace = Feed.ATOM_NS)
     public Link link;
 }
--- src/main/java/com/github/lucasskywalker64/api/youtube/model/Link.java
+++ src/main/java/com/github/lucasskywalker64/api/youtube/model/Link.java
@@ -1,6 +1,9 @@
 package com.github.lucasskywalker64.api.youtube.model;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
 public class Link {
 
+    @JacksonXmlProperty(localName = "href", isAttribute = true)
     public String href;
 }
